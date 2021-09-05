d68342c70c112a8acd20dfec3669cb6201b04ba1
melanie rat <RAT.melanie@gmail.com>
2021-09-05 17:32:29 +0200

    add attachement with shrine to form model

---
+++ app/models/file_uploader.rb
@@ -0,0 +1,2 @@
+class FileUploader < Shrine::Attachment
+end
--- app/models/form.rb
+++ app/models/form.rb
@@ -1,2 +1,7 @@
 class Form < ApplicationRecord
+	include FileUploader::Attachment(:identity_proof)
+	include FileUploader::Attachment(:address_proof)
+	include FileUploader::Attachment(:contract)
+	include FileUploader::Attachment(:bank_account_details)
+	include FileUploader::Attachment(:pay_slips)
 end
