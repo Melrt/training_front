012d2994032a4a3849e79ff86b926e1394b00e4a
melanie rat <RAT.melanie@gmail.com>
2021-09-05 18:47:15 +0200

    add constants for form model

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,4 +1,10 @@
 class Form < ApplicationRecord
+
+	CONTRACT_TYPE  = ['CDI', 'CDD', 'sans emploi', 'retraité(e)'].freeze
+	MATRIAL_STATUS = ['Marié(e)', 'divorcé(e)', 'veuf(ve)', 'célibataire'].freeze
+	HOUSING_TYPE   = ['Maison', 'Appartement'].freeze
+	STATUS         = ['Propriétaire', 'locataire', 'hébergé à titre gratuit'].freeze
+	
 	include FileUploader::Attachment(:identity_proof)
 	include FileUploader::Attachment(:address_proof)
 	include FileUploader::Attachment(:contract)
