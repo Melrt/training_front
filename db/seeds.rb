7a72a338e95591346c8e4c52e7277bcc2b7b5b1c
melanie rat <RAT.melanie@gmail.com>
2021-09-07 17:24:55 +0200

    add content to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@ puts 'Creating 3 articles'
 	article = Article.new(
 			title: Faker::Book.title,
 			publication_date: Faker::Date.backward(days: rand(1..5)),
-			category: ['BREAKING NEWS', 'FAKE NEWS'].sample
+			category: ['BREAKING NEWS', 'FAKE NEWS'].sample,
+			content: Faker::Lorem.paragraph(sentence_count: rand(10..15))
 		)
 	article.image = image
 	article.save!
