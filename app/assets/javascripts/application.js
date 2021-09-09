4f24f8a2b29dd72a1b69ef41288d708ee6a6bc85
melanie rat <RAT.melanie@gmail.com>
2021-09-09 15:38:43 +0200

    scroll reveal wip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,5 @@
 //= require rails-ujs
 //= require bootstrap/dist/js/bootstrap.bundle.min
+//= require scroll_reveal
+
+
---
+++ app/assets/javascripts/scroll_reveal.js
@@ -0,0 +1,4 @@
+window.sr = new ScrollReveal();
+sr.reveal('.form', { easing: 'ease-in', reset: true})
+sr.reveal('.payment-section', { easing: 'ease-in', reset: true })
+sr.reveal('.card', { easing: 'ease-in', reset: true })
